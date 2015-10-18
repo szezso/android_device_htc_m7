e2a08812bd5338d3d18f77bba6ab13653d307ffc
Szeleczky Zsombor <szezsoo@gmail.com>
2015-10-18 15:12:35 +0200

    M7: Merge m7 tree to m7-common

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,123 @@
+# Copyright (C) 2013 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+
+# inherit from common msm8960
+-include device/htc/msm8960-common/BoardConfigCommon.mk
+
+LOCAL_PATH := device/htc/m7
+
+TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
+
+# Kernel
+BOARD_KERNEL_BASE := 0x80600000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=qcom user_debug=31
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01800000
+TARGET_KERNEL_CONFIG := m7_defconfig
+TARGET_KERNEL_SOURCE := kernel/htc/msm8960
+
+# Audio
+BOARD_USES_FLUENCE_INCALL := true  # use DMIC in call only
+BOARD_USES_SEPERATED_AUDIO_INPUT := true  # use distinct voice recognition use case
+BOARD_USES_SEPERATED_FM := true # use distinct fm use cases
+BOARD_USES_SEPERATED_VOICE_SPEAKER := true  # use distinct voice speaker use case
+BOARD_USES_SEPERATED_VOIP := true  # use distinct VOIP use cases
+BOARD_HAVE_HTC_CSDCLIENT := true
+
+# Bluetooth
+BOARD_BLUEDROID_VENDOR_CONF := $(LOCAL_PATH)/bluetooth/libbt_vndcfg.txt
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
+BOARD_BLUETOOTH_USES_HCIATTACH_PROPERTY := false
+BOARD_HAVE_BLUETOOTH_BCM := true
+
+# Camera
+TARGET_DISPLAY_INSECURE_MM_HEAP := true
+USE_DEVICE_SPECIFIC_CAMERA := true
+
+# Charger
+BOARD_CHARGING_MODE_BOOTING_LPM := /sys/htc_lpm/lpm_mode
+
+# CMHW
+BOARD_HARDWARE_CLASS := $(LOCAL_PATH)/cmhw
+
+# Filesystem
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16776704
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+# Graphics
+OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
+HAVE_ADRENO_SOURCE := false
+
+# Recovery
+BOARD_RECOVERY_BLDRMSG_OFFSET := 2048
+COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
+TARGET_RECOVERY_DEVICE_DIRS += device/htc/m7
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
+TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+# RIL
+BOARD_PROVIDES_LIBRIL := true
+
+# SELinux
+-include device/qcom/sepolicy/sepolicy.mk
+
+BOARD_SEPOLICY_DIRS += device/htc/m7/sepolicy
+
+# USB
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
+
+# Wifi
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WLAN_DEVICE                := bcmdhd
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA          := "/system/etc/firmware/fw_bcm4335_b0.bin"
+WIFI_DRIVER_FW_PATH_AP           := "/system/etc/firmware/fw_bcm4335_apsta_b0.bin"
+WIFI_DRIVER_FW_PATH_P2P          := "/system/etc/firmware/fw_bcm4335_p2p_b0.bin"
+
+# inherit from the proprietary version
+-include vendor/htc/m7-common/BoardConfigVendor.mk
+-include vendor/htc/m7/BoardConfigVendor.mk
+
+# From M7 tree
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := m7,m7att,m7tmo,m7ul
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := m7
+
+# Filesystem
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1946156032
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 27917287424
+BOARD_CACHEIMAGE_PARTITION_SIZE := 671088128
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
---
+++ system_prop.mk
@@ -0,0 +1,39 @@
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.cam.hw.version=m7
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.opengles.version=196608 \
+    ro.sf.lcd_density=480
+
+# Input
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.input.noresample=1
+
+# NFC
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.nfc.fw_download=true \
+    debug.nfc.fw_boot_download=false \
+    debug.nfc.se=true \
+    ro.nfc.port=I2C
+
+# Performance
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.extension_library=/system/vendor/lib/libqc-opt.so
+
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.baseband.arch=mdm \
+    rild.libargs=-d /dev/smd0 \
+    rild.libpath=/system/lib/libril-qc-qmi-1.so \
+    ro.telephony.default_network=9 \
+    telephony.lteOnGsmDevice=1
+
+# Scheduler
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.io.scheduler=bfq
+
+# Time services
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.timed.enable=true
